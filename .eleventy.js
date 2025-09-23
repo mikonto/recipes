246a9fc5b475ab5cd4ff254233ad70a88902fde2
Mikael Kontolampi <mikael.kontolampi@gmail.com>
2025-09-23 13:09:48 +0200

    Fix asset paths for GitHub Pages deployment
    - Add pathPrefix configuration for /recipes/
    - Update all internal links to use url filter
    - Fix CSS and navigation links to work with subdirectory deployment

--- .eleventy.js
+++ .eleventy.js
@@ -35,6 +35,7 @@ module.exports = function(eleventyConfig) {
     },
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk"
+    dataTemplateEngine: "njk",
+    pathPrefix: "/recipes/"
   };
 };
